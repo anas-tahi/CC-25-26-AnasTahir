8f1ba65c09c4a3cf118a9efee93a40a3726689e0
anas-tahi <anastahir446@gmail.com>
2025-12-31 08:55:08 +0100

    Fix

--- Hito 4/frontend/src/services/auth.js
+++ Hito 4/frontend/src/services/auth.js
@@ -2,11 +2,11 @@
 import { authAPI } from './api';
 
 export const registerUser = async (formData) => {
-  const res = await authAPI.post('/auth/register', formData);
+  const res = await authAPI.post('/register', formData);
   return res.data;
 };
 
 export const loginUser = async (formData) => {
-  const res = await authAPI.post('/auth/login', formData);
+  const res = await authAPI.post('/login', formData);
   return res.data;
 };
