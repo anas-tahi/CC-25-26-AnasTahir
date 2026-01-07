256838a2406230450e682f44c6f8b97e7f8ecc6f
anas-tahi <anastahir446@gmail.com>
2026-01-07 06:41:16 +0100

    fix

--- Hito4/frontend/src/config.js
+++ Hito4/frontend/src/config.js
@@ -1,2 +1,3 @@
 export const PRODUCT_API_BASE = "http://localhost:5000";
-export const SHOPPING_API_BASE = "http://localhost:4000";
+export const SHOPPING_API_BASE =
+  process.env.REACT_APP_SHOPPING_API || "http://localhost:4000";
