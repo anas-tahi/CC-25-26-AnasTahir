032ecdc7044ba6c37c186f07ddab48eeee52648d
anas-tahi <anastahir446@gmail.com>
2026-01-11 02:20:43 +0100

    fix

--- Hito4/frontend/src/index.js
+++ Hito4/frontend/src/index.js
@@ -1,22 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/main.css';
 
-import { UserProvider } from "./context/UserContext";
-import { LanguageProvider } from "./context/LanguageContext";
-import { ThemeProvider } from "./context/ThemeContext";
-import { FavoritesProvider } from "./context/FavoritesContext";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider>
-    <LanguageProvider>
-      <UserProvider>
-        <FavoritesProvider>
-          <App />
-        </FavoritesProvider>
-      </UserProvider>
-    </LanguageProvider>
-  </ThemeProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
